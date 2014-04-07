0b8f33393c50cfad03e148af4c315c1d5f6512e1
user <user@10.0.55.121>
2014-04-07 16:40:19 -0500

    Richard the best

--- src/erp/ERP.java
+++ src/erp/ERP.java
@@ -16,7 +16,7 @@ public class ERP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+            System.out.println("Richard the best");
+        }
     
 }
